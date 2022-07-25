a74a7eadf35e0dda2570a603ad16b7c8a521682f
豪杰 <499650191@qq.com>
2022-07-25 10:39:12 +0800

    fix: preview page

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-uploader-solutions/' : '/',
   outputDir: 'docs',
   transpileDependencies: true,
   lintOnSave: false,
+  productionSourceMap: false,
 });
